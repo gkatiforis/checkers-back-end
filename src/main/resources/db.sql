751386b12ebd3b5f1d87bca9d3697cf5c71245ea
George Katiforis <georgekatiforis@hotmail.com>
2019-03-24 14:51:01 +0200

    Update obsolete sql script and application.properties

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -3,24 +3,9 @@ CREATE DATABASE top10;
 use  top10;
 SET CHARSET 'utf8';
 
-# DROP DATABASE IF EXISTS top;
-# CREATE DATABASE top;
-# use  top;
-# # SET CHARSET 'utf8';
-
 DROP TABLE IF EXISTS question_difficulty;
 DROP TABLE IF EXISTS answer;
-DROP TABLE IF EXISTS question;
-DROP TABLE IF EXISTS active_game_answer;
-DROP TABLE IF EXISTS active_game;
-DROP TABLE IF EXISTS  active_game_question;
-DROP TABLE IF EXISTS active_game_player;
-
 DROP TABLE IF EXISTS player;
-
-
-
-
 
 CREATE TABLE question_difficulty (
   id BIGINT PRIMARY KEY auto_increment,
@@ -43,56 +28,17 @@ CREATE TABLE answer (
   points INT,
   FOREIGN KEY (question_id) REFERENCES question(id)
 );
+
 ALTER TABLE answer CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE question CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 CREATE TABLE player (
   id BIGINT PRIMARY KEY auto_increment,
   player_id VARCHAR(200),
   username VARCHAR(200)
 );
 
-
-CREATE TABLE active_game (
-  id BIGINT PRIMARY KEY auto_increment,
-  active_player INT,
-  date_started datetime
-);
-
-
-CREATE TABLE active_game_question (
-  id BIGINT PRIMARY KEY auto_increment,
-    active_game_id BIGINT,
-  question_id BIGINT,
-   FOREIGN KEY (active_game_id) REFERENCES active_game(id),
-  FOREIGN KEY (question_id) REFERENCES question(id)
-);
-
-
-CREATE TABLE active_game_player (
-  id BIGINT PRIMARY KEY auto_increment,
-    active_game_id BIGINT,
-  player_id BIGINT,
-   FOREIGN KEY (active_game_id) REFERENCES active_game(id),
-  FOREIGN KEY (player_id) REFERENCES player(id)
-);
-
-CREATE TABLE active_game_answer (
-    id BIGINT PRIMARY KEY auto_increment,
-   answer_id BIGINT UNIQUE,
-   player_id BIGINT,
-   question_id BIGINT,
-   is_correct BOOLEAN,
-   active_game_id BIGINT,
-   FOREIGN KEY (player_id) REFERENCES player(id),
-   FOREIGN KEY (answer_id) REFERENCES answer(id),
-   FOREIGN KEY (question_id) REFERENCES question(id),
-   FOREIGN KEY (active_game_id) REFERENCES active_game(id)
-);
-
-
-
-
-
+-- Populate data
 insert into question_difficulty(description)
 values('LOW'), ('MEDIUM'), ('HARD');
 
@@ -152,7 +98,6 @@ values
 (5, 'Dixit|ντίξιτ', 1),
 (5, 'Tichu|τίτσου', 4);
 
-
 insert into answer(question_id, description, display_description,  points)
 values
   (4, 'Ψάλτης', 'Στάθης Ψάλτης', 1),
@@ -166,8 +111,6 @@ values
   (4,  'Πίκουλα','Εφη Πίκουλα', 5),
   (4,  'Μόσιος','Μιχάλης Μόσιος', 5);
 
-
-
 insert into answer(question_id, description, display_description,  points)values
   (6, 'Τυχαίο', 'Τυχαίο Δε νομίζω',  1),
   (6, 'Ομορφάντρα', 'Ομορφάντρα μου', 1),
@@ -176,48 +119,5 @@ insert into answer(question_id, description, display_description,  points)values
   (6,  'Τρατζικ', 'Τρατζικ', 4),
   (6,  'καμπάνα', 'Σήμα καμπάνα',  4);
 
-
-#   (5, 'answer 1', 1),
-#   (5, 'answer 2', 2),
-#   (5, 'answer 3', 1),
-#   (5, 'answer 4', 2),
-#   (5, 'answer 5', 2),
-#
-#   (6, 'answer 1', 1),
-#   (6, 'answer 2', 2),
-#   (6, 'answer 3', 1),
-#   (6, 'answer 4', 2),
-#   (6, 'answer 5', 2),
-#
-#   (7, 'answer 1', 1),
-#   (7, 'answer 2', 2),
-#   (7, 'answer 3', 1),
-#   (7, 'answer 4', 2),
-#   (7, 'answer 5', 2),
-#
-#   (8, 'answer 1', 1),
-#   (8, 'answer 2', 2),
-#   (8, 'answer 3', 1),
-#   (8, 'answer 4', 2),
-#   (8, 'answer 5', 2),
-#
-#   (9, 'answer 1', 1),
-#   (9, 'answer 2', 2),
-#   (9, 'answer 3', 1),
-#   (9, 'answer 4', 2),
-#   (9, 'answer 5', 2),
-#
-#   (10, 'answer 1', 1),
-#   (10, 'answer 2', 2),
-#   (10, 'answer 3', 1),
-#   (10, 'answer 4', 2),
-#   (10, 'answer 5', 2)
-;
-
-
 SELECT  * from answer;
 SELECT  * from question;
-#SELECT  * from active_game;
-SELECT  * from active_game_answer;
-#SELECT  * from active_game_question;
-#SELECT  * from active_game_player;
